6c961bf0d23d9a65c724292e454043c168683952
swathy <swathysagar.67@gmail.com>
2021-05-29 15:43:55 +0530

    imagepreview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+export class App extends React.Component {
+ 
+    state={
+      profileimg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+    }
+    imageHandler=(e)=>{
+      const reader=new FileReader();
+      reader.onload=()=>{
+        if(reader.readyState===2){
+          this.setState({profileimg: reader.result})
+        }
+      }
+      reader.readAsDataURL(e.target.files[0])
+    };
+ 
+    
+ 
+    render() {
+      const {profileimg}=this.state
+        return (
+            <div>
+              <div className='page'>
+                <div className='container'>
+                  <h1 className='heading'>Add your image..</h1>
+                  <div className='img-holder'>
+                    <img src={profileimg} alt="" id="img" className="img"/>
+                  </div>
+                  <input type="file" name="image-upload" id="input" accept="image/*" onChange={this.imageHandler}/>
+                  <div className="label">
+                    <label htmlFor="input" className="image-upload">
+                      <i className="material-icons">add_photo_alternate</i>
+                      Choose your photo
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+        );
+    }
 }
-
-export default App;
+export default App
